36f539845b6a09b97df908eea8b774ac09dc8329
Roberto Baez <roberto.jose1503@gmail.com>
2025-10-19 22:38:05 -0400

    stuff_c3

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+            }
+        });
+    });
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
